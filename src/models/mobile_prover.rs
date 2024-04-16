aecd9987c5ea963f5135481eb4902281a1ffecd4
dev-blc <genesis.dev.blc@gmail.com>
2024-04-17 02:22:35 +0530

    added rng parameter to the request

--- src/models/mobile_prover.rs
+++ src/models/mobile_prover.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
 
+use super::network::SupportedNetworks;
 use crate::errors::AvailError;
 use bincode::{deserialize, serialize};
+use snarkvm::prelude::CryptoRng;
 use snarkvm::prelude::Network;
-
-use super::network::SupportedNetworks;
+use snarkvm::prelude::Rng;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 // #[serde(bound = "N: Network")]
@@ -12,17 +13,25 @@ pub struct ProverRequest {
     pub address: String,
     pub auth_object_bytes: Vec<u8>,
     pub network: SupportedNetworks,
+    pub rng: Vec<u8>,
+    // maybe add rng
 }
 
 // trait FeeRequest
 
 // impl <N:Network> FeeRequest<N> {
 impl ProverRequest {
-    pub fn new(address: String, auth_object_bytes: Vec<u8>, network: SupportedNetworks) -> Self {
+    pub fn new(
+        address: String,
+        auth_object_bytes: Vec<u8>,
+        network: SupportedNetworks,
+        rng: Vec<u8>,
+    ) -> Self {
         Self {
             address,
             auth_object_bytes,
             network,
+            rng,
         }
     }
 
@@ -36,17 +45,30 @@ impl ProverRequest {
         Ok(data)
     }
 
-    pub async fn to_bytes_execution_object<N: Network>(
+    pub async fn to_bytes_auth_object<N: Network>(
         auth_object: snarkvm::prelude::Authorization<N>,
     ) -> Result<Vec<u8>, AvailError> {
         let bytes = serialize(&auth_object)?;
         Ok(bytes)
     }
 
-    pub async fn from_bytes_execution_object<N: Network>(
+    pub async fn from_bytes_auth_object<N: Network>(
         bytes: Vec<u8>,
     ) -> Result<snarkvm::prelude::Authorization<N>, AvailError> {
         let data: snarkvm::prelude::Authorization<N> = deserialize(&bytes)?;
+        Ok(data)
+    }
+
+    pub async fn from_bytes_rng<R: Rng + CryptoRng + serde::de::DeserializeOwned>(
+        bytes: Vec<u8>,
+    ) -> Result<R, AvailError> {
+        let data: R = deserialize(&bytes)?;
+        Ok(data)
+    }
+    pub async fn to_bytes_rng<R: Rng + CryptoRng + serde::Serialize>(
+        rng: &mut R,
+    ) -> Result<Vec<u8>, AvailError> {
+        let data: Vec<u8> = serialize(&rng)?;
         Ok(data)
     }
 }
@@ -76,17 +98,30 @@ impl ProverResponse {
         Ok(data)
     }
 
-    pub async fn to_bytes_execution_object<N: Network>(
+    pub async fn to_bytes_txn_object<N: Network>(
         txn_object: snarkvm::prelude::Transaction<N>,
     ) -> Result<Vec<u8>, AvailError> {
         let bytes = serialize(&txn_object)?;
         Ok(bytes)
     }
 
-    pub async fn from_bytes_execution_object<N: Network>(
+    pub async fn from_bytes_txn_object<N: Network>(
         bytes: Vec<u8>,
     ) -> Result<snarkvm::prelude::Transaction<N>, AvailError> {
         let data: snarkvm::prelude::Transaction<N> = deserialize(&bytes)?;
+        Ok(data)
+    }
+
+    pub async fn from_bytes_rng<R: Rng + CryptoRng + serde::de::DeserializeOwned>(
+        bytes: Vec<u8>,
+    ) -> Result<R, AvailError> {
+        let data: R = deserialize(&bytes)?;
+        Ok(data)
+    }
+    pub async fn to_bytes_rng<R: Rng + CryptoRng + serde::Serialize>(
+        rng: &mut R,
+    ) -> Result<Vec<u8>, AvailError> {
+        let data: Vec<u8> = serialize(&rng)?;
         Ok(data)
     }
 }
