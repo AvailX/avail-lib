fb61e364a9183ba354b14c258ae4146c41a58850
dev-blc <genesis.dev.blc@gmail.com>
2024-04-16 06:19:55 +0530

    mobile prover response type added

--- src/models/mobile_prover.rs
+++ src/models/mobile_prover.rs
@@ -50,3 +50,43 @@ impl ProverRequest {
         Ok(data)
     }
 }
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+// #[serde(bound = "N: Network")]
+pub struct ProverResponse {
+    txn_bytes: Vec<u8>,
+    network: SupportedNetworks,
+}
+
+// trait FeeRequest
+
+// impl <N:Network> FeeRequest<N> {
+impl ProverResponse {
+    pub fn new(txn_bytes: Vec<u8>, network: SupportedNetworks) -> Self {
+        Self { txn_bytes, network }
+    }
+
+    pub fn to_bytes(&self) -> Result<Vec<u8>, AvailError> {
+        let bytes = serialize(&self)?;
+        Ok(bytes)
+    }
+
+    pub fn from_bytes(bytes: Vec<u8>) -> Result<Self, AvailError> {
+        let data = deserialize(&bytes)?;
+        Ok(data)
+    }
+
+    pub async fn to_bytes_execution_object<N: Network>(
+        txn_object: snarkvm::prelude::Transaction<N>,
+    ) -> Result<Vec<u8>, AvailError> {
+        let bytes = serialize(&txn_object)?;
+        Ok(bytes)
+    }
+
+    pub async fn from_bytes_execution_object<N: Network>(
+        bytes: Vec<u8>,
+    ) -> Result<snarkvm::prelude::Transaction<N>, AvailError> {
+        let data: snarkvm::prelude::Transaction<N> = deserialize(&bytes)?;
+        Ok(data)
+    }
+}
