a29c0b909fc3d023489947a2b20f0e5cb9317c62
dev-blc <genesis.dev.blc@gmail.com>
2024-06-22 02:21:50 +0530

    updates owner to string

--- src/models/quests.rs
+++ src/models/quests.rs
@@ -17,7 +17,7 @@ pub struct Campaign {
     pub color: String,
     pub points_image: String,
     pub project_name: String,
-    pub owner_id: Uuid,
+    pub owner_id: String,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
