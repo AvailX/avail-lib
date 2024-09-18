a025753cd825cef99203301a6f168814dfe7a43a
dev-blc <genesis.dev.blc@gmail.com>
2024-09-19 02:38:25 +0530

    adds if localhost option

--- src/models/academy/crud_ops.rs
+++ src/models/academy/crud_ops.rs
@@ -29,61 +29,6 @@ pub async fn add_text_content_to_lesson(
     }
 }
 
-pub async fn module_exists(m_id: Uuid, is_local: bool) -> AvailResult<bool> {
-    let api = env!("TEST_API_URL");
-
-    let client = Client::new();
-    let mut req_url = format!("{api}/modules/ifExists/{m_id}");
-    if is_local {
-        req_url = format!("http://localhost:8004/modules/ifExists/{m_id}");
-    }
-    match client.get(&req_url).send().await {
-        Ok(res) => match res.json::<bool>().await {
-            Ok(result) => AvailResult::Ok(result),
-            Err(e) => Err(AvailError::new(
-                AvailErrorType::InvalidData,
-                "JSON parsing error".to_string(),
-                "JSON parsing error".to_string(),
-            )),
-        },
-        Err(e) => {
-            error!("Error sending request: {:?}", e);
-            Err(AvailError::new(
-                AvailErrorType::NotFound,
-                "Module not found in module-service".to_string(),
-                "Module not found in module-service".to_string(),
-            ))
-        }
-    }
-}
-
-pub async fn lesson_exists(l_id: Uuid, is_local: bool) -> AvailResult<bool> {
-    let client = Client::new();
-    let api = env!("TEST_API_URL");
-    let mut req_url = format!("{api}/lessons/ifExists/{l_id}");
-    if is_local {
-        req_url = format!("http://localhost:8004/lessons/ifExists/{l_id}");
-    }
-    match client.get(&req_url).send().await {
-        Ok(res) => match res.json::<bool>().await {
-            Ok(result) => AvailResult::Ok(result),
-            Err(e) => Err(AvailError::new(
-                AvailErrorType::InvalidData,
-                "JSON parsing error".to_string(),
-                "JSON parsing error".to_string(),
-            )),
-        },
-        Err(e) => {
-            error!("Error sending request: {:?}", e);
-            Err(AvailError::new(
-                AvailErrorType::NotFound,
-                "Module not found in module-service".to_string(),
-                "Module not found in module-service".to_string(),
-            ))
-        }
-    }
-}
-
 pub async fn add_lesson(m_id: Uuid, l_id: Uuid, is_local: bool) -> AvailResult<()> {
     let client = Client::new();
     let api = env!("TEST_API_URL");
--- src/models/academy/verify_utils.rs
+++ src/models/academy/verify_utils.rs
@@ -5,10 +5,14 @@ use uuid::Uuid;
 
 use super::module::Module;
 
-pub async fn module_exists(m_id: Uuid) -> AvailResult<bool> {
+pub async fn module_exists(m_id: Uuid, is_local: bool) -> AvailResult<bool> {
+    let api = env!("TEST_API_URL");
+
     let client = Client::new();
-    // let req_url = format!("https://test-api.avail.global/modules/ifExists/{m_id}");
-    let req_url = format!("http://localhost:8004/modules/ifExists/{m_id}");
+    let mut req_url = format!("{api}/modules/ifExists/{m_id}");
+    if is_local {
+        req_url = format!("http://localhost:8004/modules/ifExists/{m_id}");
+    }
     match client.get(&req_url).send().await {
         Ok(res) => match res.json::<bool>().await {
             Ok(result) => AvailResult::Ok(result),
@@ -29,11 +33,13 @@ pub async fn module_exists(m_id: Uuid) -> AvailResult<bool> {
     }
 }
 
-pub async fn lesson_exists(l_id: Uuid) -> AvailResult<bool> {
+pub async fn lesson_exists(l_id: Uuid, is_local: bool) -> AvailResult<bool> {
     let client = Client::new();
-    // let req_url = format!("https://test-api.avail.global/lessons/ifExists/{l_id}");
-    let req_url = format!("http://localhost:8004/lessons/ifExists/{l_id}");
-
+    let api = env!("TEST_API_URL");
+    let mut req_url = format!("{api}/lessons/ifExists/{l_id}");
+    if is_local {
+        req_url = format!("http://localhost:8004/lessons/ifExists/{l_id}");
+    }
     match client.get(&req_url).send().await {
         Ok(res) => match res.json::<bool>().await {
             Ok(result) => AvailResult::Ok(result),
@@ -54,11 +60,14 @@ pub async fn lesson_exists(l_id: Uuid) -> AvailResult<bool> {
     }
 }
 
-pub async fn if_lesson_is_of_module(l_id: Uuid, m_id: Uuid) -> AvailResult<bool> {
+pub async fn if_lesson_is_of_module(l_id: Uuid, m_id: Uuid, is_local: bool) -> AvailResult<bool> {
     let client = Client::new();
     // let req_url = format!("https://test-api.avail.global/modules/get/{l_id}");
-    let req_url = format!("http://localhost:8004/modules/get/{m_id}");
-
+    let api = env!("TEST_API_URL");
+    let mut req_url = format!("{api}/modules/get/{m_id}");
+    if is_local {
+        req_url = format!("http://localhost:8004/modules/get/{m_id}");
+    }
     match client.get(&req_url).send().await {
         Ok(res) => match res.json::<Module>().await {
             Ok(result) => {
