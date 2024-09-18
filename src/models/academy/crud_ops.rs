578864d57f7df60dd94c310677d7aabca8c784d8
dev-blc <genesis.dev.blc@gmail.com>
2024-09-19 03:02:17 +0530

    adds crud ops for content addition

--- src/models/academy/crud_ops.rs
+++ src/models/academy/crud_ops.rs
@@ -29,6 +29,56 @@ pub async fn add_text_content_to_lesson(
     }
 }
 
+pub async fn add_quiz_content_to_lesson(
+    module_id: Uuid,
+    lesson_id: Uuid,
+    quiz_content_id: Uuid,
+    is_local: bool,
+) -> Result<(), String> {
+    let client = reqwest::Client::new();
+    let api = env!("TEST_API_URL");
+    let mut url = format!(
+        "{}/lessons/update/quiz/{}/{}",
+        api, lesson_id, quiz_content_id
+    );
+    if is_local {
+        url = format!(
+            "http://localhost:8004/lessons/update/quiz/{}/{}",
+            lesson_id, _
+        );
+    }
+    let res = client.post(&url).send().await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
+pub async fn add_video_content_to_lesson(
+    module_id: Uuid,
+    lesson_id: Uuid,
+    video_content_id: Uuid,
+    is_local: bool,
+) -> Result<(), String> {
+    let client = reqwest::Client::new();
+    let api = env!("TEST_API_URL");
+    let mut url = format!(
+        "{}/lessons/update/video/{}/{}",
+        api, lesson_id, video_content_id
+    );
+    if is_local {
+        url = format!(
+            "http://localhost:8004/lessons/update/video/{}/{}",
+            lesson_id, video_content_id
+        );
+    }
+    let res = client.post(&url).send().await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
 pub async fn add_lesson(m_id: Uuid, l_id: Uuid, is_local: bool) -> AvailResult<()> {
     let client = Client::new();
     let api = env!("TEST_API_URL");
