e97e8a2ff3a075666ab209aecdad0140979949fa
dev-blc <genesis.dev.blc@gmail.com>
2024-09-27 06:54:33 +0530

    print shit

--- src/models/academy/crud_ops.rs
+++ src/models/academy/crud_ops.rs
@@ -333,7 +333,10 @@ pub async fn calculate_total_progress(is_local: bool, modules_completed: i32) ->
             Ok(result) => {
                 let total_modules: i32 = result.len() as i32;
                 let progress = (modules_completed / total_modules) * 100;
-                info!("progress: {:?}", progress);
+                info!(
+                    "{:?} // {:?}  ---> progress: {:?}",
+                    modules_completed, total_modules, progress
+                );
                 AvailResult::Ok(progress)
             }
             Err(e) => Err(AvailError::new(
